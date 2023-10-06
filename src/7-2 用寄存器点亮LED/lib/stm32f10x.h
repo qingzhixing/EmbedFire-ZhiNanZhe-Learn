721f0fd396876564ee66fb2fe51813245a220a75
qingzhixing <qzsptmail@protonmail.com>
2023-10-06 18:27:50 +0800

    使用结构体代替地址转指针,更加优雅

--- src/7-2 用寄存器点亮LED/lib/stm32f10x.h
+++ src/7-2 用寄存器点亮LED/lib/stm32f10x.h
@@ -9,30 +9,24 @@
 // 外设三个类型
 #define APB1_PERIPHERAL_BASE PERIPHERAL_BASE
 #define APB2_PERIPHERAL_BASE (PERIPHERAL_BASE + 0x10000)
+// 实际上AHB从SDIO(PERIPHERAL_BASE + 0x18000)开始，为了方便计算，取 + 0x20000
 #define AHB_PERIPHERAL_BASE (PERIPHERAL_BASE + 0x20000)
 
-/* GPIO结构:
-    typedef struct
-    {
-    __IO uint32_t CRL;
-    __IO uint32_t CRH;
-    __IO uint32_t IDR;
-    __IO uint32_t ODR;
-    __IO uint32_t BSRR;
-    __IO uint32_t BRR;
-    __IO uint32_t LCKR;
-    } GPIO_TypeDef;
-*/
+// GPIO结构:
+typedef struct
+{
+    uint32_t CRL;
+    uint32_t CRH;
+    uint32_t IDR;
+    uint32_t ODR;
+    uint32_t BSRR;
+    uint32_t BRR;
+    uint32_t LCKR;
+} GPIO_TypeDef;
+
 // GPIO
 #define GPIOB_BASE (APB2_PERIPHERAL_BASE + 0x0C00)
-
-// GPIO_ODR
-#define ODR_OFFSET 0x0C
-#define GPIOB_ODR_BASE (GPIOB_BASE + ODR_OFFSET)
-
-// GPIO_CRL
-#define CRL_OFFSET 0x00
-#define GPIOB_CRL_BASE (GPIOB_BASE + CRL_OFFSET)
+#define GPIOB ((GPIO_TypeDef *)GPIOB_BASE)
 
 // 时钟RCC外设地址
 #define RCC_BASE (AHB_PERIPHERAL_BASE + 0x1000)
